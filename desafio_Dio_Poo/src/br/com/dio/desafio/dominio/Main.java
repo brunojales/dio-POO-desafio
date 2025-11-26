1e2a958a8b684a0fb713ccd381799a5b7c8593ba
bruno jales <bruno.jales.90@gmail.com>
2025-11-26 15:49:33 -0300

    Tree Commit - Conteitos Iniciais

---
+++ desafio_Dio_Poo/src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,30 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Curso curso1 = new Curso();
+		curso1.setTitulo("Curso Java");
+		curso1.setDescricao("BootCamp");
+		curso1.setCargahoraria(20);
+		
+		Curso curso2 = new Curso();
+		curso2.setTitulo("Curso JavaScript");
+		curso2.setDescricao("BootCamp");
+		curso2.setCargahoraria(10);
+		
+		Mentoria mentoria1 = new Mentoria();
+		mentoria1.setTitulo("Mentoria Java");
+		mentoria1.setDescricao("BootCamp");
+		mentoria1.setData(LocalDate.now());
+		
+		System.out.println(curso1);
+		System.out.println(curso2);
+		
+		System.out.println(mentoria1);
+
+	}
+
+}
